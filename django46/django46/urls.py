f054415444b3f814f175ba98301864cd56b0f6aa
Your Name <you@example.com>
2024-03-08 00:34:43 +0500

    first commit

--- django46/django46/urls.py
+++ django46/django46/urls.py
@@ -3,11 +3,16 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
-from products.views import home_page
+from products.views import home_page, get_product_category_page, MyLoginView, logout_view, search_product
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page),
+    path('', home_page, name='home'),
+    path('shop/<int:pk>', get_product_category_page, name='category_page'),
+    path('login', MyLoginView.as_view(), name='login'),
+    path('logout', logout_view, name='logout'),
+    path('search', search_product, name='search-product'),
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
